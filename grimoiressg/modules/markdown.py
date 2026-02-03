c4b36e26d3f981a5fef1c46db32d8bde2a2d102f
sigmasternchen <git@sigma-star.io>
2026-02-03 20:08:28 +0100

    feat: Add table support

--- grimoiressg/modules/markdown.py
+++ grimoiressg/modules/markdown.py
@@ -7,4 +7,4 @@ def compile_markdown(data, context, config):
     for entry in data:
         if "markdown" in entry:
             logger.debug("Compiling markdown for %s...", entry['relative_filename'])
-            entry["markdown_compiled"] = markdown.markdown(entry["markdown"])
+            entry["markdown_compiled"] = markdown.markdown(entry["markdown"], extensions=['tables'])
